fee444081089a66666ba55636fc532c4da5a70b7
Kayala Satya Prakash <satyaprakash99915@gmail.com>
2023-08-21 23:05:26 +0530

    Update OpenRedirector.sh

--- OpenRedirector.sh
+++ OpenRedirector.sh
@@ -22,6 +22,26 @@ display_help() {
     exit 0
 }
 
+# Get the current user's home directory
+home_dir=$(eval echo ~$USER)
+
+# Check if ParamSpider is already cloned and installed
+if ! command -v paramspider &> /dev/null; then
+    echo "Installing ParamSpider..."
+    git clone https://github.com/devanshbatham/paramspider.git "$home_dir/paramspider"
+    cd "$home_dir/paramspider" || exit
+    pip install .
+fi
+
+# Check if OpenRedireX is already cloned and installed
+if ! command -v openredirex &> /dev/null; then
+    echo "Installing OpenRedireX..."
+    git clone https://github.com/devanshbatham/openredirex "$home_dir/openredirex"
+    cd "$home_dir/openredirex" || exit
+    sudo chmod +x setup.sh
+    ./setup.sh
+fi
+
 # Step 1: Parse command line arguments
 while [[ $# -gt 0 ]]
 do
@@ -44,24 +64,24 @@ done
 
 # Step 2: Ask the user to enter the domain name
 if [ -z "$domain" ]; then
-    echo "Enter the domain name: "
+    echo "Enter the domain name: target.com"
     read domain
 fi
 
 # Step 3: Get the vulnerable parameters of the given domain name using ParamSpider tool and save the output into a text file
 echo "Running ParamSpider on $domain"
-python3 /home/kali/ParamSpider/paramspider.py -d "$domain" -o /home/kali/OpenRedirector/paramspider_output.txt
+paramspider -d "$domain"
 
 # Check if ParamSpider found any unique URLs
-unique_urls=$(grep -oP '(?<=\=)[^&]+' /home/kali/OpenRedirector/paramspider_output.txt | sort -u | wc -l)
+unique_urls=$(grep -oP '(?<=\=)[^&]+' results/$domain.txt | sort -u | wc -l)
 if [ $unique_urls -eq 0 ]; then
     echo "No URLs Found"
     exit 1
 fi
 
-# Step 4: Run the openredirex.py tool on the above text file
-echo "Running openredirex.py on paramspider_output.txt"
-python3 /home/kali/OpenRedireX/openredirex.py -l /home/kali/OpenRedirector/paramspider_output.txt -p /home/kali/OpenRedireX/payloads.txt
+# Step 4: Run the OpenRedireX tool on the above text file
+echo "Running OpenRedireX on $domain.txt"
+cat results/$domain.txt | openredirex -p $home_dir/openredirex/payloads.txt -k "FUZZ" -c 50
 
 # Step 5: End with general message as the scan is completed
-echo "Scan is completed."
+echo "Scan is completed - Happy Fuzzing"
