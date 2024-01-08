09ea0448f9a5a0daeeffe8bd74104702de40ddf6
Satya Prakash <satyaprakash99915@gmail.com>
2024-01-08 17:18:19 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 # Rename the OpenRedirector.sh file to OpenRedirector
 mv OpenRedirector.sh openredirector
 
-# Move the OpenRedirector file to /usr/local/bin
-sudo mv openredirector /usr/local/bin/
+# Move the OpenRedirector file to /usr/bin
+sudo mv openredirector /usr/bin/
 
 # Make the OpenRedirector file executable
-sudo chmod +x /usr/local/bin/openredirector
+sudo chmod +x /usr/bin/openredirector
 
 echo "OpenRedirector has been installed successfully! Now Enter the command 'openredirector' to run the tool."
